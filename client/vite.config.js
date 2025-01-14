866b1dc442c9c41361fdee3f0ebdd15c48ad7cc7
swarooppatilx <swarooppatil850@gmail.com>
2025-01-14 23:32:19 +0530

    chore: minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://bit-by-query-server.vercel.app/api/", // Forward requests from /api to backend server
+      "/api": {
+        target: "https://bit-by-query-server.vercel.app/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "api"),
+      },
     },
   },
 });
