4679611dc89b16c0d77bda33b146e552015b4e40
swarooppatilx <swarooppatil850@gmail.com>
2025-01-17 13:47:58 +0530

    chore: schema.sql

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `submissions`;
+CREATE TABLE `submissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `problem_id` int(11) NOT NULL,
+  `time_taken` int(11) DEFAULT NULL,
+  `submission_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `name` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
