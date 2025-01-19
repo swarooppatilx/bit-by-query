07debef4d69f4ae0926347dd11ca1f88b7df0004
swarooppatilx <swarooppatil850@gmail.com>
2025-01-19 19:41:36 +0530

    chore: virtual contest

--- client/src/config/date.js
+++ client/src/config/date.js
@@ -1,4 +1,4 @@
-const startTime = new Date("2025-01-19T10:00:00+05:30");
-const endTime = new Date("2025-01-21T23:59:59+05:30");
+const startTime = new Date("2025-01-19T20:00:00+05:30");
+const endTime = new Date("2025-01-21T08:00:00+05:30");
 
 export { startTime, endTime };
