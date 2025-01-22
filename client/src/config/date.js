4e9e88da51d8f04d15982988796f129d8ff0155c
swarooppatilx <swarooppatil850@gmail.com>
2025-01-23 00:38:29 +0530

    chore: minor test

--- client/src/config/date.js
+++ client/src/config/date.js
@@ -1,4 +1,4 @@
-const startTime = new Date("2025-01-19T20:00:00+05:30");
-const endTime = new Date("2025-01-21T08:00:00+05:30");
+const startTime = new Date("2025-01-23T12:40:00+05:30");
+const endTime = new Date("2025-01-23T12:45:00+05:30");
 
 export { startTime, endTime };
