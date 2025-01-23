f61ef5e4ce6187b7eae7d5dfb344863d6e95b1d4
swarooppatilx <swarooppatil850@gmail.com>
2025-01-24 02:40:44 +0530

    oops

--- client/src/config/date.js
+++ client/src/config/date.js
@@ -1,4 +1,4 @@
-const startTime = new Date("2025-01-23T10:30:00+05:30");
-const endTime = new Date("2025-01-23T12:30:00+05:30");
+const startTime = new Date("2025-01-24T10:30:00+05:30");
+const endTime = new Date("2025-01-24T12:30:00+05:30");
 
 export { startTime, endTime };
