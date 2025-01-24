155b458101c87c07108388397a250fc7afadc540
swarooppatilx <swarooppatil850@gmail.com>
2025-01-24 11:00:39 +0530

    chore timer

--- client/src/config/date.js
+++ client/src/config/date.js
@@ -1,4 +1,4 @@
-const startTime = new Date("2025-01-20T10:30:00+05:30");
-const endTime = new Date("2025-01-20T12:30:00+05:30");
+const startTime = new Date("2025-01-24T11:05:00+05:30");
+const endTime = new Date("2025-01-24T13:05:00+05:30");
 
 export { startTime, endTime };
