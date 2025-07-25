3f03c0a4d9a764fa1c5cb8a1bc5a8f12d63c16cf
catmoreak <sujnanddevadiga@gmail.com>
2025-07-25 10:53:04 +0530

    feat: update date configuration to use environment variables for start and end times

--- client/src/config/date.js
+++ client/src/config/date.js
@@ -1,4 +1,5 @@
-const startTime = new Date("2025-01-01T11:05:00+05:30");
-const endTime = new Date("2026-01-01T13:05:00+05:30");
+
+const startTime = new Date(import.meta.env.VITE_START_TIME);
+const endTime = new Date(import.meta.env.VITE_END_TIME);
 
 export { startTime, endTime };
