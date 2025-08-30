659f72f262f8db13a7397efcb74362338e67c39f
swarooppatilx <swarooppatil850@gmail.com>
2025-08-30 19:16:33 +0530

    chore: update problems aug2025

--- server/utils/loadProblems.js
+++ server/utils/loadProblems.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function loadProblems() {
-  const filePath = path.join(__dirname, "..", "data", "problems", "jan2025.json");
+  const filePath = path.join(__dirname, "..", "data", "problems", "aug2025.json");
   if (fs.existsSync(filePath)) {
     return JSON.parse(fs.readFileSync(filePath, "utf-8"));
   }
