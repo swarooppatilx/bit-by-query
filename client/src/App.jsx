5bba88080641fc6de4f9de3e1739822c1c07ea59
Smart Coder <rafiaperween09@gmail.com>
2025-09-04 18:59:31 +0530

    refactor(auth): switch auth handling from localStorage to cookies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,14 @@ import CountDown from "./CountDown";
 import NotFound from "./NotFound";
 import Register from "./Register";
 import Middleware from "./middleware";
-import ForgotPassword from "./FotgotPassword";
+import ForgotPassword from "./ForgotPassword";
 import LandingPage from "./LandingPage";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
@@ -37,20 +41,13 @@ function App() {
             </Middleware>
           }
         />
-        <Route
-          path="/home"
-          element={
-            <Middleware>
-              <Home />
-            </Middleware>
-          }
-        />
         <Route path="/leaderboard" element={<LeaderBoard />} />
         <Route path="/countdown" element={<CountDown />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
+    </Provider>
   );
 }
 
